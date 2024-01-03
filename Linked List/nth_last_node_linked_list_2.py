f07a99e4184d4895d87e1303f0ec550acd75e988
CharchitChandra <109802105+charchit1208@users.noreply.github.com>
2024-01-03 23:18:02 +0530

    Update nth_last_node_linked_list_2.py

--- Linked List/nth_last_node_linked_list_2.py
+++ Linked List/nth_last_node_linked_list_2.py
@@ -1,6 +1,6 @@
 from Linked_List_Implementation import Node, LinkedList
 
-# Complete this function:
+# nth last node
 def nth_last_node(linked_list, n):
   current = None
   tail_pointer = linked_list.head_node
@@ -21,7 +21,7 @@ def generate_test_linked_list():
     linked_list.insert_beginning(i)
   return linked_list
 
-# Use this to test your code:
+# Test code:
 test_list = generate_test_linked_list()
 print(test_list.stringify_list())
 nth_last = nth_last_node(test_list, 4)
